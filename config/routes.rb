e48c42b72887515b4f5043dc0be10ad5e18037dd
DennisDaddy <softteez95@gmail.com>
2017-04-29 23:22:28 +0300

    generate controller posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'posts/new'
+
+  get 'posts/index'
+
+  get 'posts/show'
+
+  get 'posts/edit'
+
+  get 'posts/update'
+
+  get 'posts/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
